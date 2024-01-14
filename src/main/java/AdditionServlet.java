df5085921ead61e181f6e5a71ff415f147d9edf7
Eden Asamere <edenasamere77@gmail.com>
2024-01-14 04:36:35 -0800

    first

---
+++ src/main/java/AdditionServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/AdditionServlet")
+public class AdditionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int number1 = Integer.parseInt(request.getParameter("number1"));
+        int number2 = Integer.parseInt(request.getParameter("number2"));
+        
+        int result = number1 + number2;
+        
+        response.getWriter().write("<h1>Result: " + result + "</h1>");
+    }
+}
